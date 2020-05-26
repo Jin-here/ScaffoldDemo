bc31023217b27e6706a2ae28ae2f2768b6c89a52
caojin <350194214@qq.com>
2020-05-26 15:13:26 +0800

    fix bug

--- app/src/main/java/com/vgaw/scaffolddemo/page/feedback/FeedbackAc.kt
+++ app/src/main/java/com/vgaw/scaffolddemo/page/feedback/FeedbackAc.kt
@@ -3,12 +3,14 @@ package com.vgaw.scaffolddemo.page.feedback
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.vgaw.scaffold.page.ScaffoldAc
 import com.vgaw.scaffold.util.statusbar.StatusBarUtil
+import com.vgaw.scaffold.view.AppToast
 import com.vgaw.scaffold.view.webview.FileChooserWebChromClient
 import com.vgaw.scaffolddemo.R
 import kotlinx.android.synthetic.main.feedback_ac.*
@@ -38,7 +40,16 @@ class FeedbackAc : ScaffoldAc() {
         feedbackWv.settings.domStorageEnabled = true
         feedbackWv.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                return super.shouldOverrideUrlLoading(view, url)
+                if (url != null && url.startsWith("weixin://")) {
+                    try {
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                        view!!.context.startActivity(intent)
+                    } catch (e: Exception) {
+                        AppToast.show(R.string.feedback_wx_not_installed)
+                    }
+                    return true
+                }
+                return false
             }
         }
         mFileChooserWebChromClient = FileChooserWebChromClient(getSelf())
